804b01d77c0d2aec8f8bc44b238192e8b10c0a52
Tayyaba Ali <98578632+tayyaba-ali@users.noreply.github.com>
2023-03-27 16:43:35 +0500

    npx tailwind init filename --full

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['*'],
+	theme: {
+		extend: {
+			fontSize: {
+				'10xl': ['10rem', { lineHeight: '1.6' }],
+			},
+			spacing: {
+				13: '4rem',
+			},
+		},
+	},
+	plugins: [],
+};
